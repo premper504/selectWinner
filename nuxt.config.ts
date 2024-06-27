6f9f333a5a81df99c2772c6d986a400b40f11454
RMou <ronyjmolina@gmail.com>
2024-06-27 12:27:39 -0600

    env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,6 +66,6 @@ export default defineNuxtConfig({
     public: {
       SUPABASE_URL: process.env.SUPABASE_URL,
       SUPABASE_KEY: process.env.SUPABASE_KEY,
-    },
+    }
   }
 })
--- plugins/supabaseClient.js
+++ plugins/supabaseClient.js
@@ -7,10 +7,13 @@ export default defineNuxtPlugin((nuxtApp) => {
   const supabaseUrl = config.public.SUPABASE_URL
   const supabaseKey = config.public.SUPABASE_KEY
 
+  console.log('Supabase URL:', supabaseUrl)
+  console.log('Supabase Key:', supabaseKey)
+
   if (!supabaseUrl || !supabaseKey) {
     console.error('No se pudo acceder alas variables de entorno!')
   } else {
     const supabase = createClient(supabaseUrl, supabaseKey)
     nuxtApp.provide('supabase', supabase)
   }
-}) 
+})
