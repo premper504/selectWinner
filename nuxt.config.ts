6230144374bbb28054aac5dee402d5bb2375f5f8
RMou <ronyjmolina@gmail.com>
2026-01-09 14:36:49 -0600

    config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,15 +6,15 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Tómbola Sorteo Lufussa 2025',
+      title: 'Sorteo Ceteco 2026',
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Tómbola Sorteo Lufussa 2025' }
+        { name: 'description', content: 'Sorteo Ceteco 2026' }
       ],
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/x-icon', href: '/assets/images/favicon.ico' },
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
